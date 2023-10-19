bdc1edd31685657f1babe5a73a38efe29e578d63
trishia31 <115858503+trishia31@users.noreply.github.com>
2023-10-19 22:21:30 +0800

    update index.html, interest-media.css, interest-script.js, interest-style.css, and media.css
     index.html, interest-media.css, interest-script.js, interest-style.css, and media.css

--- interest-script.js
+++ interest-script.js
@@ -1,25 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sidemenu = document.getElementById("sidemenu");
-    const menuButton = document.getElementById("menuButton");
-    const closeButton = document.getElementById("closeButton");
-    const navMenu = document.querySelector('nav ul');
+  const sidemenu = document.getElementById("sidemenu");
+  const menuButton = document.getElementById("menuButton");
+  const closeButton = document.getElementById("closeButton");
+  const navMenu = document.querySelector("nav ul");
 
-    function openmenu() {
-        sidemenu.style.right = "0";
-    }
+  function openmenu() {
+    sidemenu.style.right = "0";
+    closeButton.style.display = "block"; // Show the close button
+    menuButton.style.display = "none"; // Hide the menu button
+  }
 
-    function closemenu() {
-        sidemenu.style.right = "-200px";
-    }
+  function closemenu() {
+    sidemenu.style.right = "-200px";
+    closeButton.style.display = "none"; // Hide the close button
+    menuButton.style.display = "block"; // Show the menu button
+  }
 
-    function openMenu() {
-        navMenu.classList.add('show-menu');
-    }
+  function openMenu() {
+    navMenu.classList.add("show-menu");
+  }
 
-    function closeMenu() {
-        navMenu.classList.remove('show-menu');
-    }
+  function closeMenu() {
+    navMenu.classList.remove("show-menu");
+  }
 
-    menuButton.addEventListener('click', openMenu);
-    closeButton.addEventListener('click', closeMenu);
+  menuButton.addEventListener("click", openMenu);
+  closeButton.addEventListener("click", closeMenu);
 });
